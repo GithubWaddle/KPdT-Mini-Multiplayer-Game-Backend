829042bbc63a340e0e19c98f1fab757f51a2ccc3
gamerzord <gamerzord76@gmail.com>
2026-06-30 15:32:09 +0000

    grpc is torture

---
+++ main/grpc/generated/Matchmaking/RankingServiceClient.php
@@ -0,0 +1,48 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+namespace Matchmaking;
+
+/**
+ * What matchmaking can ask ranking service to do
+ */
+class RankingServiceClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * @param \Matchmaking\UpdateRankingRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall<\Matchmaking\UpdateRankingResponse>
+     */
+    public function UpdateRanking(\Matchmaking\UpdateRankingRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/matchmaking.RankingService/UpdateRanking',
+        $argument,
+        ['\Matchmaking\UpdateRankingResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Matchmaking\GetPlayerRankRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall<\Matchmaking\GetPlayerRankResponse>
+     */
+    public function GetPlayerRank(\Matchmaking\GetPlayerRankRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/matchmaking.RankingService/GetPlayerRank',
+        $argument,
+        ['\Matchmaking\GetPlayerRankResponse', 'decode'],
+        $metadata, $options);
+    }
+
+}
